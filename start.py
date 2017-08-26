43417a58206952069397541be8f6a09118db3032
バナナギコ <Ei.pri.bird-in-angelsladder@hotmail.co.jp>
2017-08-26 09:22:35 +0900

    file io exec
    ファイルを読み込んで内容を表示
    値:Decisionを判定しbool値を反転
    反転した新しいjsonデータをtestdata.jsonに書き込み

--- start.py
+++ start.py
@@ -4,10 +4,25 @@ import json
 #読み込み
 f = open("testdata.json")
 jsonData = json.load(f)
-str = json.dumps(jsonData,ensure_ascii=False,indent=4)
-print(str)
+st = json.dumps(jsonData,ensure_ascii=False,indent=4)
+print(st)
+
+#判定
+bool = True
+if jsonData["Decision"]:
+    bool = False
+else:
+    bool = True
+
+#表示
+print("")
+print("Now Decision ...")
+#https://detail.chiebukuro.yahoo.co.jp/qa/question_detail/q12174941742
+print("Decision : "+ str(jsonData["Decision"]) +" -> "+str(bool))
 
 #編集
-
+jsonData["Decision"] = bool
 
 #書き込み
+f = open("testdata.json", "w")
+json.dump(jsonData, f, ensure_ascii=False,indent=4)
